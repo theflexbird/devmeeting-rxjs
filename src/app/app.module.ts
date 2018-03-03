7fab3db3dc6efc3913d7295664de8160a66c7d7f
Mateusz Myszka <mateusz@whitebird.pl>
2018-03-03 11:04:56 +0100

    First step - working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { environment } from '../environments/environment';
 
 
 @NgModule({
   imports: [
     BrowserModule,
-    AngularFireModule.initializeApp(environment.firebase, 'rxjslab')
+    AngularFireModule.initializeApp(environment.firebase, 'rxjslab'),
+    AngularFirestoreModule,
   ],
   declarations: [ AppComponent ],
   bootstrap: [ AppComponent ]
