c74e67c35a91d0702423e2b44e7f6f3bbe1099f1
Mateusz Myszka <mateusz@whitebird.pl>
2018-03-04 20:55:12 +0100

    Step three - A2

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -2,8 +2,9 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { Model } from './message.model';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/retry';
 
 @Injectable()
 export class MessageService {
@@ -12,7 +13,9 @@ export class MessageService {
 
   constructor(private db: AngularFirestore) {
     this.collection = db.collection<Model>('messages', ref => ref.orderBy('timestamp'));
-    this.messages = this.collection.valueChanges().catch((error, source$) => {
+    this.messages = this.collection.valueChanges()
+    .retry(5)
+    .catch((error) => {
       console.error('Oops:', error.message);
       return Observable.of([]);
     });
